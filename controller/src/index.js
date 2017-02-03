c1a6a9667c9e7e96c1727b4f1efff74fe8824a39
Ed Eustace <ed.eustace@gmail.com>
2017-02-03 17:07:04 +0000

    Step 4: Adding data to the model

---
+++ controller/src/index.js
@@ -0,0 +1,15 @@
+export function outcome(question, session) {
+
+  return new Promise((resolve) => {
+    resolve({});
+  });
+
+}
+
+export function model(question, session, env) {
+
+  return new Promise((resolve) => {
+    resolve(question);
+  });
+
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ export default class Toggle extends HTMLElement {
 
   constructor() {
     super();
-    this.innerHTML = `<div> hello, world </div>`;
+    this._model = null;
+    this._session = null;
+  }
+
+  set model(m) {
+    this._model = m;
+    this._rerender();
+  }
+
+  set session(s) {
+    this._session = s;
+    this._rerender();
+  }
+
+  get session() {
+    return this._session;
+  }
+
+  _message() {
+    return this._model ? this._model.message : 'hello, world';
+  }
+
+  _rerender() {
+    this.innerHTML = `<div>${this._message()}</div>`;
+  }
+
+  connectedCallback() {
+    this.dispatchEvent(new CustomEvent('pie.register', { bubbles: true }));
+    this._rerender();
   }
 
 }
